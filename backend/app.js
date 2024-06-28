12306122e4115eb758c5538c2c57ba4d1497b8fc
harsham02 <harshavardhan15015@gmail.com>
2024-06-29 00:05:00 +0530

    second-commit

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,13 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 
+
+app.get("/", (req, res) => {
+    res.json({
+        success : true,
+        message: "HELLO WORLD!",
+    });
+});
 app.use("/api/v1/message",messageRouter);
 dbConnection();
 
